dfb7e85752986399de3d7ff021b6f5be4f265440
BuildACloud <sonnguyen.2009@gmail.com>
2025-11-19 22:10:52 +0100

    charger sommets

--- TP2_carte_son.py
+++ TP2_carte_son.py
@@ -9,6 +9,29 @@ def charger_csv_en_matrice(nom_fichier):
                 matrice.append(ligne)
     return matrice
 
+def charger_sommets(matrice_position):
+    sommets = {}
+    
+    # Noter l'indexe de chaque champ
+    header = matrice_position[0]
+    idx_ville = header.index('ville')
+    idx_x = header.index('posX')
+    idx_y = header.index('posY')
+
+    # Noter chaque ville à partir de la 2ème ligne et ajouter dans le dictionnaire
+    for ligne in matrice_position[1:]:
+        ville = ligne[idx_ville]
+        x = float(ligne[idx_x])
+        y = float(ligne[idx_y])
+        sommets[ville] = (x, y)
+    
+    return sommets
+
 if __name__ == "__main__":
     matrice = charger_csv_en_matrice("TP2_position.csv")
     print(matrice)
+
+    sommets = charger_sommets(matrice)
+    print(sommets)
+
+    
